2c9b5a1f47d45ca54f908ed036521a3552821ffb
sparrara <quinonezn@zippyttech.com>
2021-05-07 15:02:04 -0400

    login verificando

---
+++ src/app/modulos/login/auth.service.ts
@@ -0,0 +1,24 @@
+//import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { DoCheck, Injectable } from '@angular/core';
+
+//import { AngularFireAuth } from '@angular/fire/auth';
+import { HttpService } from '../../http/httpServices/http.service'
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+//import { auth } from 'firebase/app';
+import { environment } from '../../../environments/environment'
+//import firebase from '@firebase/app';
+
+export interface User {
+  uid: string;
+  email: string;
+
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+ 
+
+}
